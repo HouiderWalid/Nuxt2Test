c7f3320f46af93d53a192e2d6c42f1d752ad6bf1
Houider <houiderwalid@gmail.com>
2023-02-23 22:30:09 +0100

    push .nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
-  //target: 'static',
+
   head: {
     titleTemplate: '%s - Nuxt2Test',
     title: 'Nuxt2Test',
